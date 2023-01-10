4812d8dd6fb9accf53b926eb3ca2a59ecc8b900f
MahendraReddyK2428 <118844400+MahendraReddyK2428@users.noreply.github.com>
2023-01-10 18:04:20 +0530

    Add files via upload

---
+++ src/Components/BtnComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function BtnComponent(props) {
+  return (
+    <div>
+      {(props.status === 0)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-gre"
+        onClick={props.start}>Start</button> 
+        <button className="stopwatch-btn stopwatch-btn-yel-disabled"
+                  >Reset</button>
+        </div>: ""
+        
+      }
+
+      {(props.status === 1)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-red"
+                  onClick={props.stop}>Pause</button>
+          <button className="stopwatch-btn stopwatch-btn-yel"
+                  onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
+
+     {(props.status === 2)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-gre"
+                  onClick={props.resume}>Resume</button>
+          <button className="stopwatch-btn stopwatch-btn-yel"
+                  onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
+     
+    </div>
+  );
+}
+
+export default BtnComponent;
